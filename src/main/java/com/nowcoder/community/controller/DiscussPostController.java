25d23d01d1542a039ae6e535891cc08bb533cace
worture <worture@163.com>
2022-07-14 21:29:40 +0800

    补传帖子相关控制层代码

---
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -0,0 +1,63 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.pojo.DiscussPost;
+import com.nowcoder.community.pojo.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping(path = "/discuss")
+public class DiscussPostController {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String addDiscussPost(String title, String content) {
+        User user = hostHolder.getUser();
+        if (user == null) {
+            return CommunityUtil.getJSONString(403,"你还没登录!");
+        }
+
+        DiscussPost discussPost = new DiscussPost();
+        discussPost.setUserId(user.getId());
+        discussPost.setTitle(title);
+        discussPost.setContent(content);
+        discussPost.setCreateTime(new Date());
+        discussPostService.addDiscussPost(discussPost);
+
+        //报错以后统一处理
+        return CommunityUtil.getJSONString(0,"发布成功!");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        //查询id对应的帖子
+        DiscussPost discussPost = discussPostService.selectDiscussPostById(discussPostId);
+        model.addAttribute("post", discussPost);
+
+        //查询帖子作者信息
+        User user = userService.getUserById(discussPost.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
+    }
+
+}
